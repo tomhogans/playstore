3df66ab88c6939deecdb199b852234476236c478
Tom Hogans <tomhsx@gmail.com>
2012-11-27 10:56:47 -0500

    Add jarsigner script

---
+++ sign-apk-with-debug-key.sh
@@ -0,0 +1,1 @@
+jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android -digestalg SHA1 -sigalg MD5withRSA -sigfile CERT -signedjar $1-signed.apk $1.apk androiddebugkey
